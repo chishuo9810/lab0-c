64fdc13d85ec69a8562182a174b7f51d8d424572
chishuo9810 <chishuo9810@gmail.com>
2024-03-03 05:31:46 +0800

    Implement "q_reverse" and add condition in q_swap
    Reverse elements in queue.
    Add NULL judge in "q_swap".

--- queue.c
+++ queue.c
@@ -157,6 +157,9 @@ bool q_delete_dup(struct list_head *head)
 /* Swap every two adjacent nodes */
 void q_swap(struct list_head *head)
 {
+    if (!head || list_empty(head)) {
+        return;
+    }
     struct list_head *current, *future;
     list_for_each_safe (current, future, head) {
         while (future != head && current != head) {
@@ -174,7 +177,18 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+    struct list_head *current, *future;
+    list_for_each_safe (current, future, head) {
+        current->next = current->prev;
+        current->prev = future;
+    }
+    current->next = current->prev;
+    current->prev = future;
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
