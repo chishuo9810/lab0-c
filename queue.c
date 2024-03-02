a54a437b740da3f70abd0d83daef5c1c85d5f2c4
chishuo9810 <chishuo9810@gmail.com>
2024-03-02 16:52:46 +0800

    Implement "q_remove_head" and "q_remove_tail"
    Before remove,check if head,list,and sp is empty.
    Use memcpy to copy the removed value to sp.

--- queue.c
+++ queue.c
@@ -60,13 +60,27 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head) || !sp) {
+        return NULL;
+    }
+    element_t *removed_node = list_first_entry(head, element_t, list);
+    list_del(head->next);
+    memcpy(sp, removed_node->value, bufsize);
+    sp[bufsize - 1] = '\0';
+    return removed_node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head) || !sp) {
+        return NULL;
+    }
+    element_t *removed_node = list_last_entry(head, element_t, list);
+    list_del(head->prev);
+    memcpy(sp, removed_node->value, bufsize);
+    sp[bufsize - 1] = '\0';
+    return removed_node;
 }
 
 /* Return number of elements in queue */
